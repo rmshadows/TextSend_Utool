e50b0e25e580d6fb9788844718b0524db185b732
ryan <ryan@>
2023-05-11 05:41:28 +0800

    update —— 2023年 05月 11日 星期四 05:41:28 CST

--- debug.js
+++ debug.js
@@ -1,6 +1,12 @@
+"use strict";
 var m_system = require("./m_system.js");
 
-// console.log("Hello World");
+console.log("Hello World");
+var a = [1,2,3]
+a[4] = 4
+a.forEach(e => {
+    console.log(e);
+});
 
-m_system.helloWorld();
+// m_system.getIPs()
 
--- m_system.js
+++ m_system.js
@@ -1,4 +1,3 @@
-// import Clipboard from 'clipboard';
 
 /**
  * 测试函数
@@ -7,18 +6,42 @@ function helloWorld(){
     console.log("Hello World.");
 }
 
+
+function getIPs(){
+    const os = require('os');
+    // 设定一个局域网的默认值
+    // let localWlanHost = 'localhost';
+    let localWlanHost = [];
+    try {
+        // networkInterfaces这个方法详见：http://nodejs.cn/api/os.html#os_os_networkinterfaces
+        const ifaces = os.networkInterfaces();
+        for (let dev in ifaces) {
+            ifaces[dev].forEach((details, alias) => {
+            // 寻找IPv4协议族，并且地址不是本地地址或者回环地址的地址即可。
+            if (details.family === 'IPv4' && details.address !== '127.0.0.1' && !details.internal) {
+                console.log(details.address);
+                // localWlanHost = details.address;
+                // localWlanHost.join(details.address);
+            }
+        });
+    }
+    } catch (e) {
+        console.log(e);
+        localWlanHost = 'localhost';
+    }
+    console.log(localWlanHost);
+}
+
+
 /**
  * 复制到剪辑版
  * @returns 
  */
 function copyText (text) {
     try {
-        const btnCopy = new Clipboard('btn');
-        this.copyValue = 'hello world';
+        utools.copyText(text)
     } catch (e) {
         return false
-    }finally{
-        btn.destroy()
     }
     return true
 }
@@ -43,5 +66,6 @@ function pasteText () {
 module.exports = {
     helloWorld,
     copyText,
-    pasteText
+    pasteText,
+    getIPs
 }
