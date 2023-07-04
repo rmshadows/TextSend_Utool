e918a4bb3baac2f93d78920fd4c2d6f6c26830be
許景欣 <xjx0106@gmail.com>
2023-07-04 11:09:34 +0800

    feat: 中间部分组件抽取

--- textsend_vue/src/App.vue
+++ textsend_vue/src/App.vue
@@ -9,23 +9,15 @@
 //   NLayoutSider,
 //   NLayoutFooter,
 // } from "naive-ui";
-import HeaderComponent from '@/components/HeaderComponent.vue';
-import QrComponent from "./components/Qr_component.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import MainBodyComponent from "@/components/MainBodyComponent.vue";
 import FooterComponent from "./components/Footer_component.vue";
-import TextsendComponent from "./components/Textsend_component.vue";
 </script>
 
 <template>
   <div class="textsend">
     <HeaderComponent />
-    <div class="textsend-body">
-      <div class="textsend-body-left">
-        <QrComponent />
-      </div>
-      <div class="textsend-body-right">
-        <TextsendComponent />
-      </div>
-    </div>
+    <MainBodyComponent />
     <FooterComponent />
   </div>
 </template>
@@ -34,27 +26,10 @@ import TextsendComponent from "./components/Textsend_component.vue";
 .textsend {
   width: 100%;
   height: 100vh;
-  background-color: rgb(242, 242, 242);
-  
-  .textsend-body {
-    /* clear: both; */
-    width: 100%;
-    display: flex;
-    height: calc((100vh - 80px - 70px));
-
-    .textsend-body-left {
-      width: calc((100vh - 80px - 70px));
-      height: 100%;
-      background-color: rgb(207, 255, 192);
-    }
-    .textsend-body-right {
-      width: calc(100vw - (100vh - 80px - 70px));
-      height: 100%;
-      background-color: rgb(250, 255, 153);
-    }
-  }
+  background-color: rgb(242, 242, 242);  
 }
 </style>
+
 <style>
 body {
   margin: 0px;
---
+++ textsend_vue/src/components/MainBodyComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="textsend-body">
+    <div class="textsend-body-left">
+      <QrComponent />
+    </div>
+    <div class="textsend-body-right">
+      <TextSendComponent />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import QrComponent from "./Qr_component.vue";
+import TextSendComponent from "./Textsend_component.vue";
+</script>
+
+<style scoped lang="less">
+.textsend-body {
+  /* clear: both; */
+  width: 100%;
+  display: flex;
+  height: calc((100vh - 80px - 70px));
+
+  .textsend-body-left {
+    width: calc((100vh - 80px - 70px));
+    height: 100%;
+    background-color: rgb(207, 255, 192);
+  }
+  .textsend-body-right {
+    width: calc(100vw - (100vh - 80px - 70px));
+    height: 100%;
+    background-color: rgb(250, 255, 153);
+  }
+}
+</style>
